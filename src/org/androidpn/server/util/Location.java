f584a6d16ddf8c3ec76cba38a4fe7becdea4c1b6
qianzf <qianzf@tenzont.com>
2018-02-08 16:03:07 +0800

    修改

--- src/org/androidpn/server/util/Location.java
+++ src/org/androidpn/server/util/Location.java
@@ -2,12 +2,31 @@ package org.androidpn.server.util;
 
 public class Location {
 
+    private double longitude;
+    private double latitude;
+
     public Location () {
 
     }
 
     public Location (String location) {
 
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
     }
 
     @Override
